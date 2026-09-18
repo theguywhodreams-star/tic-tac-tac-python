1128705105608cc153dca4eb55f561cb1319f312
theguywhodreams-star <swagatghosh2307@gmail.com>
2026-09-18 17:23:27 +0530

    Create thebase.py
    this is day 1 and i have made the base for the game

---
+++ thebase.py
@@ -0,0 +1,7 @@
+# defining the rows and coloums in advance if i need to use it ever
+m, n = (3, 3)
+# setting the board using lists of three and underscores as space
+TT = [['_' for i in range (n)] for j in range(m)]
+for tt in TT:
+  print(tt)
+# that concludes making the base
